6470b9c06b66ff77cbce9f16760df83f0f1b62f6
DmitriyBabij <132613323+DmitriyBabij@users.noreply.github.com>
2025-01-21 13:55:14 +0200

    hw

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,18 +1,26 @@
-const API_KEY = "47491725-6916c20f65c7c72c223d91484";
-const BASE_URL = "https://pixabay.com/api/";
+import axios from 'axios';
 
-export function fetchImages(query) {
-  const url = `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
+const API_KEY = '47491725-6916c20f65c7c72c223d91484';
+const BASE_URL = 'https://pixabay.com/api/';
 
-  return fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .catch(error => {
-      console.error('Error fetching images:', error);
-      throw error;
-    });
+let page = 1; // Початкове значення для пагінації
+
+export async function fetchImages(query) {
+  const url = `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=15`;
+
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error;
+  }
+}
+
+export function incrementPage() {
+  page += 1;
+}
+
+export function resetPage() {
+  page = 1;
 }
